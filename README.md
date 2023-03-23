4609b4f5e7abb8f511793a03869966826d7e8433
Étienne Lévesque <contact@etiennel.dev>
2023-03-22 22:33:32 -0400

    Start lexical

--- README.md
+++ README.md
@@ -1,71 +1,25 @@
 # lexical README
 
-This is the README for your extension "lexical". After writing up a brief description, we recommend including the following sections.
+## Starting the extension
 
-## Features
+The extension is not yet published to the Visual Studio Marketplace. The extension must be built and installed manually.
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+### Configuring vscode-lexical
 
-For example if there is an image subfolder under your extension project workspace:
+vscode-lexical currently does not package lexical itself.
 
-\!\[feature X\]\(images/feature-x.png\)
+First, clone the lexical repo and follow the instructions in the README to build a release.
 
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+Once that's done, create a file called `lexical_release_path.txt` at the root of the project and write to it the absolute path to your lexical release.
 
-## Requirements
+It should look something like `/home/username/Projects/lexical/_build/dev/rel`.
 
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
+### Starting the extension
 
-## Extension Settings
-
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
-This extension contributes the following settings:
-
-* `myExtension.enable`: Enable/disable this extension.
-* `myExtension.thing`: Set to `blah` to do something.
-
-## Known Issues
-
-Calling out known issues can help limit users opening duplicate issues against your extension.
-
-## Release Notes
-
-Users appreciate release notes as you update your extension.
-
-### 1.0.0
-
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
----
+Press F5. VSCode should build and start the extension.
 
 ## Following extension guidelines
 
 Ensure that you've read through the extensions guidelines and follow the best practices for creating your extension.
 
-* [Extension Guidelines](https://code.visualstudio.com/api/references/extension-guidelines)
-
-## Working with Markdown
-
-You can author your README using Visual Studio Code. Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux).
-* Toggle preview (`Shift+Cmd+V` on macOS or `Shift+Ctrl+V` on Windows and Linux).
-* Press `Ctrl+Space` (Windows, Linux, macOS) to see a list of Markdown snippets.
-
-## For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
+- [Extension Guidelines](https://code.visualstudio.com/api/references/extension-guidelines)
--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,67 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { readFile, readFileSync } from "fs";
+import { join } from "path";
+import { ExtensionContext, window } from "vscode";
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+} from "vscode-languageclient/node";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "lexical" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('lexical.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Lexical!');
-	});
-
-	context.subscriptions.push(disposable);
+export function activate(context: ExtensionContext) {
+  startLexical(context);
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
+
+async function startLexical(context: ExtensionContext) {
+  const outputChannel = window.createOutputChannel("Lexical");
+
+  const releaseDirectory = readFileSync(
+    context.asAbsolutePath("./lexical_release_path.txt")
+  )
+    .toString()
+    .trim();
+
+  const serverOptions: ServerOptions = {
+    command: join(releaseDirectory, "lexical/start_lexical.sh"),
+  };
+
+  const clientOptions: LanguageClientOptions = {
+    outputChannel,
+    // Register the server for Elixir documents
+    // the client will iterate through this list and chose the first matching element
+    documentSelector: [
+      { language: "elixir", scheme: "file" },
+      { language: "elixir", scheme: "untitled" },
+      { language: "eex", scheme: "file" },
+      { language: "eex", scheme: "untitled" },
+      { language: "html-eex", scheme: "file" },
+      { language: "html-eex", scheme: "untitled" },
+      { language: "phoenix-heex", scheme: "file" },
+      { language: "phoenix-heex", scheme: "untitled" },
+      { language: "surface", scheme: "file" },
+      { language: "surface", scheme: "untitled" },
+    ],
+  };
+
+  const client = new LanguageClient(
+    "lexical",
+    "Lexical",
+    serverOptions,
+    clientOptions
+  );
+
+  await client
+    .start()
+    .then(() => {
+      console.log("Started Lexical");
+    })
+    .catch((reason) => {
+      window.showWarningMessage(`Failed to start Lexical: ${reason}`);
+    });
+}
