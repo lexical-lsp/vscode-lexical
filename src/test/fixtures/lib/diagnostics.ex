f82adb5cb4a99f8b2576e61b54cdf70ce1e29a04
Étienne Lévesque <contact@etiennel.dev>
2023-03-31 17:37:56 -0400

    test: Add server start smoke test

---
+++ src/test/fixtures/lib/diagnostics.ex
@@ -0,0 +1,5 @@
+defmodule Fixtures do
+  def hello do
+    foo()
+  end
+end
---
+++ src/test/helpers.ts
@@ -0,0 +1,33 @@
+import * as vscode from 'vscode';
+import * as path from 'path';
+
+export enum Fixture {
+	diagnostics = 'diagnostics'
+}
+
+/**
+ * Activates the vscode.lsp-sample extension
+ */
+export async function activate(fixture: Fixture): Promise<[vscode.TextDocument, vscode.TextEditor]> {
+	const fixturesProjectPath = path.resolve(__dirname, './fixtures');
+
+	// The extensionId is `publisher.name` from package.json
+	const ext = vscode.extensions.getExtension('lexical-lsp.lexical')!;
+	await ext.activate();
+	try {
+		const fixtureFilePath = path.resolve(fixturesProjectPath, './lib/', `${fixture}.ex`);
+		console.log(fixtureFilePath);
+		const doc = await vscode.workspace.openTextDocument(fixtureFilePath);
+		const editor = await vscode.window.showTextDocument(doc);
+		await sleep(1500); // Wait for server activation
+
+		return [doc, editor];
+	} catch (e) {
+		console.error(e);
+		throw e;
+	}
+}
+
+async function sleep(ms: number) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
