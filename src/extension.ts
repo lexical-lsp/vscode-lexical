fcbf1caa4c33342e1a6a66a6b02a7df2910877c9
Étienne Lévesque <contact@etiennel.dev>
2023-04-04 20:57:39 -0400

    create language server module

--- src/extension.ts
+++ src/extension.ts
@@ -8,60 +8,13 @@ import {
   LanguageClientOptions,
   ServerOptions,
 } from "vscode-languageclient/node";
+import * as LanguageServer from "./language-server";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: ExtensionContext) {
-  startLexical(context);
+  LanguageServer.start(context);
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
-
-async function startLexical(context: ExtensionContext) {
-  const outputChannel = window.createOutputChannel("Lexical");
-
-  const releaseDirectory = readFileSync(
-    context.asAbsolutePath("./lexical_release_path.txt")
-  )
-    .toString()
-    .trim();
-
-  const serverOptions: ServerOptions = {
-    command: join(releaseDirectory, "lexical", "start_lexical.sh"),
-  };
-
-  const clientOptions: LanguageClientOptions = {
-    outputChannel,
-    // Register the server for Elixir documents
-    // the client will iterate through this list and chose the first matching element
-    documentSelector: [
-      { language: "elixir", scheme: "file" },
-      { language: "elixir", scheme: "untitled" },
-      { language: "eex", scheme: "file" },
-      { language: "eex", scheme: "untitled" },
-      { language: "html-eex", scheme: "file" },
-      { language: "html-eex", scheme: "untitled" },
-      { language: "phoenix-heex", scheme: "file" },
-      { language: "phoenix-heex", scheme: "untitled" },
-      { language: "surface", scheme: "file" },
-      { language: "surface", scheme: "untitled" },
-    ],
-  };
-
-  const client = new LanguageClient(
-    "lexical",
-    "Lexical",
-    serverOptions,
-    clientOptions
-  );
-
-  await client
-    .start()
-    .then(() => {
-      console.log("Started Lexical");
-    })
-    .catch((reason) => {
-      window.showWarningMessage(`Failed to start Lexical: ${reason}`);
-    });
-}
---
+++ src/language-server.ts
@@ -0,0 +1,52 @@
+import { readFileSync } from "fs";
+import { join } from "path";
+import { ExtensionContext, window } from "vscode";
+import { LanguageClient, LanguageClientOptions, ServerOptions } from "vscode-languageclient/node";
+
+export async function start(context: ExtensionContext) {
+  const outputChannel = window.createOutputChannel("Lexical");
+
+  const releaseDirectory = readFileSync(
+    context.asAbsolutePath("./lexical_release_path.txt")
+  )
+    .toString()
+    .trim();
+
+  const serverOptions: ServerOptions = {
+    command: join(releaseDirectory, "lexical", "start_lexical.sh"),
+  };
+
+  const clientOptions: LanguageClientOptions = {
+    outputChannel,
+    // Register the server for Elixir documents
+    // the client will iterate through this list and chose the first matching element
+    documentSelector: [
+      { language: "elixir", scheme: "file" },
+      { language: "elixir", scheme: "untitled" },
+      { language: "eex", scheme: "file" },
+      { language: "eex", scheme: "untitled" },
+      { language: "html-eex", scheme: "file" },
+      { language: "html-eex", scheme: "untitled" },
+      { language: "phoenix-heex", scheme: "file" },
+      { language: "phoenix-heex", scheme: "untitled" },
+      { language: "surface", scheme: "file" },
+      { language: "surface", scheme: "untitled" },
+    ],
+  };
+
+  const client = new LanguageClient(
+    "lexical",
+    "Lexical",
+    serverOptions,
+    clientOptions
+  );
+
+  await client
+    .start()
+    .then(() => {
+      console.log("Started Lexical");
+    })
+    .catch((reason) => {
+      window.showWarningMessage(`Failed to start Lexical: ${reason}`);
+    });
+}
