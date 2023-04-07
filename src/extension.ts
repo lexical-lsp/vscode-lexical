d1c4459e285f9bb9eeaa75631be2d12a2f5a0962
Étienne Lévesque <contact@etiennel.dev>
2023-04-07 18:52:28 -0400

    auto-install lexical on activation

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,13 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import { readFile, readFileSync } from "fs";
-import { join } from "path";
 import { ExtensionContext, window } from "vscode";
-import {
-  LanguageClient,
-  LanguageClientOptions,
-  ServerOptions,
-} from "vscode-languageclient/node";
 import * as LanguageServer from "./language-server";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: ExtensionContext) {
-  LanguageServer.start(context);
+export function activate(context: ExtensionContext): void {
+	LanguageServer.install(context).then(() => LanguageServer.start(context));
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate(): void {}
--- src/language-server.ts
+++ src/language-server.ts
@@ -1,52 +1,107 @@
-import { readFileSync } from "fs";
-import { join } from "path";
-import { ExtensionContext, window } from "vscode";
+import axios from "axios";
+import * as fs from "fs";
+import { ExtensionContext, ProgressLocation, Uri, window } from "vscode";
 import { LanguageClient, LanguageClientOptions, ServerOptions } from "vscode-languageclient/node";
+import * as unzipper from 'unzipper';
 
-export async function start(context: ExtensionContext) {
-  const outputChannel = window.createOutputChannel("Lexical");
+export async function start(context: ExtensionContext): Promise<void> {
+	const outputChannel = window.createOutputChannel("Lexical");
 
-  const releaseDirectory = readFileSync(
-    context.asAbsolutePath("./lexical_release_path.txt")
-  )
-    .toString()
-    .trim();
+	const startScriptPath = Uri.joinPath(getLexicalReleaseUri(context), "start_lexical.sh").fsPath;
 
-  const serverOptions: ServerOptions = {
-    command: join(releaseDirectory, "lexical", "start_lexical.sh"),
-  };
+	const serverOptions: ServerOptions = {
+		command: startScriptPath,
+	};
 
-  const clientOptions: LanguageClientOptions = {
-    outputChannel,
-    // Register the server for Elixir documents
-    // the client will iterate through this list and chose the first matching element
-    documentSelector: [
-      { language: "elixir", scheme: "file" },
-      { language: "elixir", scheme: "untitled" },
-      { language: "eex", scheme: "file" },
-      { language: "eex", scheme: "untitled" },
-      { language: "html-eex", scheme: "file" },
-      { language: "html-eex", scheme: "untitled" },
-      { language: "phoenix-heex", scheme: "file" },
-      { language: "phoenix-heex", scheme: "untitled" },
-      { language: "surface", scheme: "file" },
-      { language: "surface", scheme: "untitled" },
-    ],
-  };
+	const clientOptions: LanguageClientOptions = {
+		outputChannel,
+		// Register the server for Elixir documents
+		// the client will iterate through this list and chose the first matching element
+		documentSelector: [
+			{ language: "elixir", scheme: "file" },
+			{ language: "elixir", scheme: "untitled" },
+			{ language: "eex", scheme: "file" },
+			{ language: "eex", scheme: "untitled" },
+			{ language: "html-eex", scheme: "file" },
+			{ language: "html-eex", scheme: "untitled" },
+			{ language: "phoenix-heex", scheme: "file" },
+			{ language: "phoenix-heex", scheme: "untitled" },
+			{ language: "surface", scheme: "file" },
+			{ language: "surface", scheme: "untitled" },
+		],
+	};
 
-  const client = new LanguageClient(
-    "lexical",
-    "Lexical",
-    serverOptions,
-    clientOptions
-  );
+	const client = new LanguageClient(
+		"lexical",
+		"Lexical",
+		serverOptions,
+		clientOptions
+	);
 
-  await client
-    .start()
-    .then(() => {
-      console.log("Started Lexical");
-    })
-    .catch((reason) => {
-      window.showWarningMessage(`Failed to start Lexical: ${reason}`);
-    });
+	await client
+		.start()
+		.then(() => {
+			console.log("Started Lexical");
+		})
+		.catch((reason) => {
+			window.showWarningMessage(`Failed to start Lexical: ${reason}`);
+		});
+}
+
+export async function install(context: ExtensionContext): Promise<void> {
+	return window.withProgress({
+		title: 'Installing Lexical server...',
+		location: ProgressLocation.Notification
+	}, async progress => {
+		const lexicalZipUri = getLexicalZipUri(context);
+		const lexicalReleaseUri = getLexicalReleaseUri(context);
+
+		ensureInstallationDirectoryExists(context);
+    
+		progress.report({ message: 'Downloading Lexical executable'});
+
+		await downloadZip(lexicalZipUri);
+
+		progress.report({ message: 'Installing...'});
+
+		extractZip(lexicalZipUri, lexicalReleaseUri);
+	});
+}
+
+function getLexicalInstallationDirectoryUri(context: ExtensionContext): Uri {
+	return Uri.joinPath(context.globalStorageUri, "lexical_install");
+}
+
+function getLexicalZipUri(context: ExtensionContext): Uri {
+	const installDirUri = getLexicalInstallationDirectoryUri(context);
+	return Uri.joinPath(installDirUri, `lexical.zip`);
+}
+
+function getLexicalReleaseUri(context: ExtensionContext): Uri {
+	const installDirUri = getLexicalInstallationDirectoryUri(context);
+	return Uri.joinPath(installDirUri, `lexical`);
+}
+
+async function downloadZip(zipUri: Uri): Promise<void> {
+	const response = await axios.get(`https://lexical-release.s3.ca-central-1.amazonaws.com/lexical.zip`, { responseType: 'arraybuffer'});
+
+	fs.writeFileSync(zipUri.fsPath, response.data, 'binary');
+}
+
+function ensureInstallationDirectoryExists(context: ExtensionContext): void {
+	const installDirUri = getLexicalInstallationDirectoryUri(context);
+
+	if (!fs.existsSync(installDirUri.fsPath)) {
+		fs.mkdirSync(installDirUri.fsPath);
+	}
+}
+
+function extractZip(zipUri: Uri, releaseUri: Uri): void {
+	fs.createReadStream(zipUri.fsPath)
+		.pipe(unzipper.Extract({ path: releaseUri.fsPath }));
+
+	fs.chmodSync(Uri.joinPath(releaseUri, 'start_lexical.sh').fsPath, 0o755);
+	fs.chmodSync(Uri.joinPath(releaseUri, 'bin', 'lexical').fsPath, 0o755);
+	fs.chmodSync(Uri.joinPath(releaseUri, 'releases', '0.1.0', 'elixir').fsPath, 0o755);
+	fs.chmodSync(Uri.joinPath(releaseUri, 'releases', '0.1.0', 'iex').fsPath, 0o755);
 }
