fb67bf6fada84e2a783504c81ed892fd8f6608e3
Étienne Lévesque <contact@etiennel.dev>
2023-05-21 10:57:19 -0400

    format

--- src/test-e2e/vscode-environment.js
+++ src/test-e2e/vscode-environment.js
@@ -3,15 +3,15 @@ const { TestEnvironment } = require("jest-environment-node");
 const vscode = require("vscode");
 
 class VsCodeEnvironment extends TestEnvironment {
-  async setup() {
-    await super.setup();
-    this.global.vscode = vscode;
-  }
+	async setup() {
+		await super.setup();
+		this.global.vscode = vscode;
+	}
 
-  async teardown() {
-    this.global.vscode = {};
-    await super.teardown();
-  }
+	async teardown() {
+		this.global.vscode = {};
+		await super.teardown();
+	}
 }
 
 module.exports = VsCodeEnvironment;
