099b274e5accd9540d73d6564159a824b8cd910b
Étienne Lévesque <contact@etiennel.dev>
2023-05-21 09:22:29 -0400

    migrate unit tests config to TS

--- jest.e2e.config.js
+++ jest.e2e.config.js
@@ -1,11 +1,12 @@
-// see https://github.com/microsoft/vscode-test/issues/37#issuecomment-700167820
 const path = require("path");
 
 module.exports = {
 	moduleFileExtensions: ["js"],
+	clearMocks: true,
+	injectGlobals: false,
+	showSeed: true,
 	testMatch: ["<rootDir>/out/test-e2e/suite/**.test.js"],
 	testEnvironment: "./src/test-e2e/vscode-environment.js",
-	verbose: true,
 	moduleNameMapper: {
 		vscode: path.join(__dirname, "src", "test-e2e", "vscode.js"),
 	},
