7c6b7bb23034c4e4d0490da951dab269f955a803
Étienne Lévesque <contact@etiennel.dev>
2023-06-19 21:50:43 -0400

    test: Add installation manifest module tests (#28)

--- __mocks__/vscode.ts
+++ __mocks__/vscode.ts
@@ -1,4 +1,8 @@
 import { URI } from "vscode-uri";
 
+function joinPath(base: URI, ...pathSegments: string[]): URI {
+	return base.with({ path: [base.path, ...pathSegments].join("/") });
+}
+
 // eslint-disable-next-line @typescript-eslint/naming-convention
-export const Uri = { parse: URI.parse };
+export const Uri = { parse: URI.parse, joinPath };
