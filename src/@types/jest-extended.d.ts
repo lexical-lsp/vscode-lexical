f57f01a3308b375997a283cbf23185ff6599970d
Étienne Lévesque <contact@etiennel.dev>
2023-10-11 17:07:37 -0400

    update jest-extended

--- src/@types/jest-extended.d.ts
+++ src/@types/jest-extended.d.ts
@@ -5,10 +5,5 @@ type JestExtendedMatchers = typeof matchers;
 
 declare module "@jest/expect" {
 	// eslint-disable-next-line @typescript-eslint/no-empty-interface, @typescript-eslint/no-unused-vars
-	export interface Matchers<R> extends JestExtendedMatchers {
-		toHaveBeenCalledBefore(
-			mock: jest.MockedFunction<any>,
-			failIfNoSecondInvocation?: boolean
-		): void;
-	}
+	export interface Matchers<R> extends JestExtendedMatchers {}
 }
