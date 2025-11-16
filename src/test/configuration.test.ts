b5066f836e34af7a9bb71bd1b424c3a56dcb9c72
Étienne Lévesque <contact@etiennel.dev>
2025-11-16 11:21:29 -0500

    add tests

--- src/test/configuration.test.ts
+++ src/test/configuration.test.ts
@@ -25,4 +25,45 @@ describe("Configuration", () => {
 
 		expect(projectDirUri).toEqual(URI.file("/stub/subdirectory"));
 	});
+
+	test("getReleasePathOverride returns undefined when no releasePathOverride is configured", () => {
+		const getConfigMock = jest.fn().mockReturnValue(undefined);
+		const workspace = WorkspaceFixture.withUri(URI.file("/stub"));
+		const releasePathOverride = Configuration.getReleasePathOverride(
+			getConfigMock,
+			workspace,
+		);
+
+		expect(releasePathOverride).toBeUndefined();
+	});
+
+	test("getReleasePathOverride returns the path as configured when it is asolute", () => {
+		const absolutePath = "/an/absolute/path";
+		const getConfigMock = jest.fn().mockReturnValue(absolutePath);
+		const workspace = WorkspaceFixture.withUri(URI.file("/stub"));
+		const releasePathOverride = Configuration.getReleasePathOverride(
+			getConfigMock,
+			workspace,
+		);
+
+		expect(releasePathOverride).toBe(absolutePath);
+	});
+
+	test.each([
+		["./a/relative/path", "/my/workspace", "/my/workspace/a/relative/path"],
+		["../a/relative/path", "/my/workspace", "/my/a/relative/path"],
+		["a/relative/path", "/my/workspace", "/my/workspace/a/relative/path"],
+	])(
+		"getReleasePathOverride returns the workspace path joined with the release path when it is relative (%s)",
+		(releasePath, workspacePath, expectedPath) => {
+			const getConfigMock = jest.fn().mockReturnValue(releasePath);
+			const workspace = WorkspaceFixture.withUri(URI.file(workspacePath));
+			const releasePathOverride = Configuration.getReleasePathOverride(
+				getConfigMock,
+				workspace,
+			);
+
+			expect(releasePathOverride).toBe(expectedPath);
+		},
+	);
 });
